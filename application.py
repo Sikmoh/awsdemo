446d75a8c5ff18fa4ca137e4eba74bd8d9ddbd65
Sikmoh <mohsikiru@gmail.com>
2022-06-14 12:58:38 +0000

    demo

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+
+@application.route('/start')
+def hello_world():
+    intro = 'this is a test'
+    return intro
+
+
+if __name__ == '__main__':
+    hello_world()
+    application.run(host="127.0.0.1", port=5003)
